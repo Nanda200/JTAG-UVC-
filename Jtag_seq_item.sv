4cb6580164df016b3c93c1726856e0f5f5cb6c77
Nanda200 <75884706+Nanda200@users.noreply.github.com>
2023-12-07 10:48:52 +0530

    Update Jtag_seq_item.sv

--- Jtag_seq_item.sv
+++ Jtag_seq_item.sv
@@ -5,87 +5,106 @@ class jtag_seq_item extends uvm_sequence_item;
   rand bit tms[]; //tms pattern 
   rand bit tdi[]; //tdi 
   rand jtag_ir inst;
+  rand int ds_dly;
+  rand int irs_dly;
   bit tdo[];
   
+   
  
  
-    //include field macro automation if AUTOMATE is defined,else use userdefined hooks
+  //include field macro automation if AUTOMATE is defined,else use userdefined hooks
   
-  //`uvm_object_utils(jtag_seq_item)
+  `uvm_object_utils(jtag_seq_item)
   
- 
-   //`ifdef AUTOMATE 
+  `ifdef AUTOMATE 
   
   `uvm_object_utils_begin(jtag_seq_item)
-  `uvm_field_enum(jtag_ir, inst, UVM_DEFAULT)
-    `uvm_field_array_int(tms, UVM_DEFAULT)
-    `uvm_field_array_int(tdi, UVM_DEFAULT)
-    `uvm_field_array_int(tdo, UVM_DEFAULT)
+     `uvm_field_enum(jtag_ir, inst, UVM_DEFAULT)
+     `uvm_field_array_int(tms, UVM_DEFAULT)
+     `uvm_field_array_int(tdi, UVM_DEFAULT)
+     `uvm_field_array_int(tdo, UVM_DEFAULT)
+     `uvm_field_int(ds_dly,UVM_DEFAULT)
+     `uvm_field_int(irs_dly,UVM_DEFAULT)
   `uvm_object_utils_end
   
-// `endif
+ `endif
   
+  //fixing the size of tdi to 10
+  constraint size_con {soft tdi.size() == 10;
+                     }
+ 
+  /*//constrainig delay to max 5 cycles
+  constraint delay_con1 {ds_dly > 0;
+                        ds_dly < 5; }
+  constraint delay_con2 {irs_dly > 0;
+                         irs_dly < 5; }*/
+  
+  constraint delay_con2 {ds_dly == 0;
+                         irs_dly == 0;}
+ 
   
    function new(string name = "jtag_seq_item");
-    super.new(name);
-  endfunction
+     super.new(name);
+   endfunction
  
-  
-   //fixing the size of tdi to 10
-  constraint con {tdi.size() == 10;}
-  
-  
-   virtual function string convert2string();
-    
-     string contents;
-     $sformat(contents, "tms_pattern = %0p,tdi = %0p,inst= %0s,tdo = %0p", tms, tdi, inst.name,tdo);
-      return contents;
-    
-   endfunction : convert2string
- 
-/*    
+
  //--------------------------------------------------------
  //callback methods
  //--------------------------------------------------------
+   //callback hook for print method
    virtual function void do_print(uvm_printer printer);
-     super.do_print(printer);
-     foreach(tms[i])
-       printer.print_field_int($sformatf("tms[%0d]", i), data[i], $bits(data[i]), UVM_HEX);
-     printer.print_field("data=", data, $bits(data), UVM_HEX);
-     printer.print_field("addr=", addr, $bits(addr), UVM_HEX);
-     printer.print_string("wr_rd", wr_rd.name);
-     printer.print_string("delay_type", delay.name);
-     printer.print_field("delay_val", delay_val, $bits(delay_val));
-                         
-    
-  endfunction : do_print 
-  
-  
-  
-   virtual function void do_copy(uvm_object rhs);
      
-      jtag_seq_item item;
-      super.do_copy(rhs);
-      $cast(item, rhs);
-      tms = item.tms;
-      tdi = item.tdi;
-      inst = item.inst;
-      tdo = item.tdo;
-    
-   endfunction : do_copy
+     super.do_print(printer);
+     
+     foreach(tms[i])
+       printer.print_field_int($sformatf("tms[%0d]", i), tms[i], $bits(tms[i]), UVM_BIN);
+     foreach(tdi[i])
+       printer.print_field_int($sformatf("tdi[%0d]", i), tdi[i], $bits(tdi[i]), UVM_BIN);
+     foreach(tdo[i])
+       printer.print_field_int($sformatf("tdo[%0d]", i), tdo[i], $bits(tdo[i]), UVM_BIN);
+     
+     printer.print_string("INSTRUCTION", inst.name); 
+     printer.print_field_int("DS_DELAY",ds_dly,$bits(ds_dly),UVM_DEC);
+     printer.print_field_int("IRS_DELAY",irs_dly,$bits(irs_dly),UVM_DEC);
+   endfunction : do_print 
+  
+  
+  //callback hook for copy method
+  virtual function void do_copy(uvm_object rhs);
+    jtag_seq_item item;
+    super.do_copy(rhs);
+    $cast(item, rhs);
+    tms = item.tms;
+    tdi = item.tdi;
+    inst = item.inst;
+    tdo = item.tdo;    
+  endfunction : do_copy
   
    // User defined callback of compare method
-   virtual function bit do_compare (uvm_object rhs, uvm_comparer comparer);
-      jtag_seq_item item;
-      bit comp;
-      $cast(item, rhs);
-      comp = super.do_compare(item, comparer) &
-     (tms == item.tms) & (tdi == item.tdi) & (inst == item.inst) &(tdo == item.tdo);
-      return comp;
+   virtual function bit do_compare (uvm_object rhs,uvm_comparer comparer);
+     jtag_seq_item item;
+     bit idcode[32];    
+     bit comp;
+     
+     $cast(item, rhs);
+     
+     if(item.inst == IDCODE) begin
+       {>>{idcode}} = `IDCODE_VALUE;
+      // $display("idcode val :%p",idcode);
+       comp = super.do_compare(item,comparer) & (inst == item.inst) & (item.tdo == idcode);
+     end
+     else begin
+       comp = super.do_compare(item, comparer) & (tdi == item.tdo) & (inst == item.inst) ;
+     end
+     return comp;
    endfunction : do_compare
    
-  */
    
-  
+  virtual function string convert2string();
+    string contents;
+    $sformat(contents, "tms_pattern = %p,tdi = %p,inst= %b,tdo = %p", tms, tdi, inst,tdo);
+    return contents;
+  endfunction : convert2string
+ 
   
 endclass:jtag_seq_item
