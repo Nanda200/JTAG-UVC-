0b09e48265b6e5af0bfa74b07d50ddfea5ac8b52
Nanda200 <75884706+Nanda200@users.noreply.github.com>
2023-12-07 10:51:25 +0530

    Update jtag_env.sv

--- jtag_env.sv
+++ jtag_env.sv
@@ -4,7 +4,8 @@ class jtag_env extends uvm_env;
   // agent and scoreboard instance
   //---------------------------------------
   jtag_agent      j_agnt;
-//  jtag_scoreboard j_scb;
+  jtag_scoreboard j_scb;
+  jtag_fcov j_cov;
   
   `uvm_component_utils(jtag_env)
   
@@ -22,15 +23,17 @@ class jtag_env extends uvm_env;
     super.build_phase(phase);
 
     j_agnt = jtag_agent::type_id::create("j_agnt", this);
-   // j_scb = jtag_scoreboard::type_id::create("j_scb", this);
-    
+    j_scb = jtag_scoreboard::type_id::create("j_scb", this);
+    j_cov = jtag_fcov::type_id::create("j_cov",this);
   endfunction : build_phase
   
   //---------------------------------------
   // connect_phase - connecting monitor and scoreboard port
   //---------------------------------------
   function void connect_phase(uvm_phase phase);
-    //j_agnt.mon.item_collected_port.connect(j_scb.item_collected_export);
+    j_agnt.mon.in_item_collected_port.connect(j_scb.in_item_collected_export);
+    j_agnt.mon.out_item_collected_port.connect(j_scb.out_item_collected_export);
+    scb.cov_port.connect(j_cov.analysis_export);
   endfunction : connect_phase
 
 endclass : jtag_env
